f17dfdf26a84698db1bd782df722cd055ed02650
Nath-G <nathalie.greselin.com>
2020-12-11 19:19:13 +0100

    lost .h

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nagresel <nagresel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/25 14:36:20 by nagresel          #+#    #+#             */
+/*   Updated: 2020/05/07 18:27:21 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+
+char	*ft_str_join(char *s1, char *s2);
+int		get_next_line(int fd, char **line);
+
+#endif
